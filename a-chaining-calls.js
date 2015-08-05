ce8e2904c23b29a65dc243ba8bce59b9659b2add
Haneef Abdul Karim <haneefx7@gmail.com>
2015-08-05 08:39:42 +0800

    1st code - chaining calls

--- a-chaining-calls.js
+++ a-chaining-calls.js
@@ -1,15 +1,40 @@
 var fs = require('fs')
+var path = require('path')
 
-var getFileContent = function (err, content) {
-    
-    if (err) {
-        console.error(err)
-    } else {
-        console.log('Content of this file:\n\n%s', content)
-    }
+// __dirname returns the current directory this file resides in
+
+var dir = path.join(__dirname, 'a-chaining-calls')
+
+// __filename returns the absolute path for the current file
+
+var file = __filename
+var target = path.join(dir, 'write-target')
+
+
+var handleError = function (err) {
+    console.error(err)
 }
 
-// __filename returns the absolute path of this file
+fs.mkdir(dir, function (err) {
+    
+    if (err) {
+        handleError(err)
+    } else {
+        fs.readFile(file, function (err, content) {
+            if (err) {
+                handleError(err)
+            } else {
+                fs.writeFile(target, content, function (err) {
+                    if (err) {
+                        handleError(err)
+                    } else {
+                        console.log('Write completed successfully')
+                    }
+                })
+            }
+        })
+    }
+})
 
-fs.readFile(__filename, { encoding: 'utf8' }, getFileContent)
+
 
