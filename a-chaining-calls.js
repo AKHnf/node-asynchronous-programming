105d63c785782090e63f982a8d56c5f0c0a21acc
Haneef Abdul Karim <haneefx7@gmail.com>
2015-08-05 23:23:59 +0800

    1st - Chaining without callback hell and generalizing errors

--- a-chaining-calls.js
+++ a-chaining-calls.js
@@ -1,40 +1,38 @@
 var fs = require('fs')
 var path = require('path')
 
-// __dirname returns the current directory this file resides in
-
 var dir = path.join(__dirname, 'a-chaining-calls')
-
-// __filename returns the absolute path for the current file
-
-var file = __filename
 var target = path.join(dir, 'write-target')
+var file = __filename
 
-
-var handleError = function (err) {
+var logError = function (err) {
     console.error(err)
 }
 
-fs.mkdir(dir, function (err) {
-    
-    if (err) {
-        handleError(err)
-    } else {
-        fs.readFile(file, function (err, content) {
-            if (err) {
-                handleError(err)
-            } else {
-                fs.writeFile(target, content, function (err) {
-                    if (err) {
-                        handleError(err)
-                    } else {
-                        console.log('Write completed successfully')
-                    }
-                })
-            }
-        })
+var handleError = function (callback) {
+    return function (err, result) {
+        if (err) {
+            logError(err)
+        } else {
+            callback(result)
+        }
     }
-})
+}
 
+var haveFile = function (content) {
+    fs.writeFile(target, content, handleError(verifyWrite))
+}
+
+var verifyWrite = function () {
+    console.log('Write completed successfully')
+}
+
+var writeContentToFile = function () {
+    fs.readFile(file, handleError(haveFile))
+}
+
+fs.mkdir(dir, handleError(writeContentToFile))
+    
+    
 
 
